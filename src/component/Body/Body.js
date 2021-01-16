4140ceb21da62e9498af7ea0459c49342ebafc80
abhis018 <76279317+abhis018@users.noreply.github.com>
2021-01-16 13:00:27 +0530

    adding images and styling to the landing page
    Signed-off-by: abhis018 <76279317+abhis018@users.noreply.github.com>

--- src/component/Body/Body.js
+++ src/component/Body/Body.js
@@ -4,6 +4,9 @@ import hero from './hero.svg';
 import img1 from './pic1.svg';
 import img2 from './pic2.svg';
 import img3 from './pic3.svg';
+import logo1 from './chrome.svg';
+import logo2 from './firefox.svg';
+import logo3 from './opera.svg';
 
 const Body = () => {
     return (
@@ -54,6 +57,77 @@ const Body = () => {
                     <button className="mv4 bg-blue ba b--near-none br2 h2 w-30 b white grow shadow-2 f7">More Info</button>
                 </div>
             </div>
+            <div className="tc">
+                <h1>Download the extension</h1>
+                We’ve got more browsers in the pipeline. Please do let us know if you’ve 
+                got a favourite you’d like us to prioritize.
+            </div>
+            <div class="main">
+                <div class="rec1 shadow-5 br2 grow">
+                    <img src={logo1} width="70px"/>
+                    <div>
+                        <h3>Add to Chrome</h3>
+                        <p>Minimum version 62</p>
+                        <button className="click bg-blue ba b--near-none white br2 f7 h2 w-100">Add & Install Extension</button>
+                    </div>
+                </div>
+                <div class="rec2 shadow-5 br2 grow">
+                    <img src={logo2} width="70px"/>
+                    <div>
+                        <h3>Add to Firefox</h3>
+                        <p>Minimum version 55</p>
+                        <button className="click bg-blue ba b--near-none white br2 f7 h2 w-100">Add & Install Extension</button>
+                    </div>
+                </div>
+                <div class="rec3 shadow-5 br2 grow">
+                    <img src={logo3} width="70px"/>
+                    <div>
+                        <h3>Add to Opera</h3>
+                        <p>Minimum version 46</p>
+                        <button className="click bg-blue ba b--near-none white br2 f7 h2 w-100">Add & Install Extension</button>
+                    </div>
+                </div>
+            </div>
+            <div className="ques tc ml7 mr7">
+                <h2>Frequently Asked Questions</h2>
+                
+                <p>Here are some of our FAQs. If you have any other questions<br/> you’d like 
+                answered please feel free to email us.</p>
+
+                {/* <!-- Question 1 --> */}
+                What is Bookmark?
+
+                {/* <!-- Answer 1 --> */}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt 
+                justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.
+
+                {/* <!-- Question 2 --> */}
+                How can I request a new browser?
+
+                {/* <!-- Answer 2 --> */}
+                Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. 
+                Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, 
+                ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. 
+                Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.
+
+                {/* <!-- Question 3 --> */}
+                Is there a mobile app?
+
+                {/* <!-- Answer 3 --> */}
+                Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum 
+                urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed 
+                sollicitudin ex et ultricies bibendum.
+
+                {/* <!-- Question 4 --> */}
+                What about other Chromium browsers?
+
+                {/* <!-- Answer 4 --> */}
+                Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam 
+                vitae neque eget nisl gravida pellentesque non ut velit.
+
+                <button className="mv4 bg-blue ba b--near-none br2 h2 w-10 b white grow shadow-2 f7">More Info</button>
+            </div>
+            
         </div>
     )
 }
